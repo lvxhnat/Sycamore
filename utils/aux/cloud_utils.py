771747bb6d21f30ce4660f1bb94872fd0fcbec84
Yi Kuang Loh <yikuang5@gmail.com>
2022-01-29 18:20:36 +0800

    edit: configure cloud storage write to env variables

--- utils/aux/cloud_utils.py
+++ utils/aux/cloud_utils.py
@@ -15,14 +15,14 @@ dotenv.load_dotenv()
 class CloudUtility:
 
     def __init__(self, ):
-        self.credentials_url = os.environ["GOOGLE_CLOUD_CREDENTIALS"]
-        self.project_id = ''  # e.g. root-sanctuary-178203
-        self.bucket_name = ''  # e.g. synthesisbucket
+        # e.g. rising-woods-284132
+        self.project_id = os.environ['GOOGLE_PROJECT_ID']
+        # e.g. synthesisbucket
+        self.bucket_name = os.environ['GOOGLE_BUCKET_NAME']
+        self.credentials_url = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
 
         self.credentials = service_account.Credentials.from_service_account_file(
             self.credentials_url)
-
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = self.credentials_url
 
         self.client = bigquery.Client(
             credentials=self.credentials, project=self.project_id)
@@ -33,30 +33,22 @@ class CloudUtility:
         self.fs = gcsfs.GCSFileSystem(
             project=self.project_id, token=os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
 
-    def get_files_with_prefix_from_gcs(self, prefix="", delimiter=None):
+    def get_files_with_prefix_from_gcs(self, bucket_name = os.environ['GOOGLE_BUCKET_NAME'], prefix="", delimiter=None):
         storage_client = storage.Client()
         bucket = storage_client.get_bucket(self.bucket_name)
         blobs = bucket.list_blobs(prefix=prefix, delimiter=delimiter)
         blob_names = [blob.name for blob in blobs]
         return blob_names
 
-    def read_file_simple_from_gcs(self, filepath):
-        try:
-            with self.fs.open(filepath) as f:
-                return pd.read_csv(f, sep="\t", encoding="utf8")
-        except:
-            logging.info(filepath)
-            return None
-
-    def read_files_from_gcs(self, folder, prefix="", delimiter=None):
+    def read_files_from_gcs(self, folder, prefix="", sep="\t", delimiter=None):
         read_base = "gs://" + self.bucket_name + "/"
-        blobs = self.get_files_with_prefix(self.bucket_name, prefix=folder)
+        blobs = self.get_files_with_prefix_from_gcs(self.bucket_name, prefix=folder)
         all_data = pd.DataFrame()
         temp = pd.DataFrame()
         files_done = 0
         for blob in blobs:
             with self.fs.open(read_base + blob) as f:
-                df = pd.read_csv(f, sep="\t", encoding="utf8")
+                df = pd.read_csv(f, sep=sep, encoding="utf8")
                 temp = pd.concat([temp, df])
                 if(files_done % 1000 == 0):
                     all_data = pd.concat([all_data, temp])
@@ -66,9 +58,9 @@ class CloudUtility:
         all_data = pd.concat([all_data, temp])
         return all_data
 
-    def write_files_to_gcs(self, df, write_path):
-        with self.fs.open("gs://" + self.bucket + "/" + write_path, "w") as f:
-            df.to_csv(f, encoding="utf8", index=None, sep="\t")
+    def write_files_to_gcs(self, df, write_path, sep="\t"):
+        with self.fs.open("gs://" + self.bucket_name + "/" + write_path, "w") as f:
+            df.to_csv(f, encoding="utf8", index=None, sep=sep)
 
     @staticmethod
     def query_from_bq(query):
