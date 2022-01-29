bc8657a1fcae698a7f354116bd7128852bea9f5d
Yi Kuang Loh <yikuang5@gmail.com>
2022-01-29 17:34:58 +0800

    fix: MASTER_SECRET_KEY env variable corrupted to become DJANGO_SECRET_KEY

--- utils/auth/auth_utils.py
+++ utils/auth/auth_utils.py
@@ -2,36 +2,43 @@ import jwt
 import os
 
 import bcrypt
-from pymongo import MongoClient 
+from pymongo import MongoClient
 
 from dotenv import load_dotenv
-from datetime import datetime, timedelta 
+from datetime import datetime, timedelta
 
 load_dotenv()
 
+
 def generate_token(username: str):
-    payload = lambda x: {
-        "exp": datetime.timestamp(datetime.now() + timedelta(hours = x)),
+    def payload(x): return {
+        "exp": datetime.timestamp(datetime.now() + timedelta(hours=x)),
         "user": username,
-            }
-    access_token = jwt.encode(payload(int(os.environ['ACCESS_KEY_LIFETIME'])), os.environ['MASTER_SECRET_KEY'])
+    }
+    access_token = jwt.encode(payload(
+        int(os.environ['ACCESS_KEY_LIFETIME'])), os.environ['MASTER_SECRET_KEY'])
     return access_token
+
 
 def verify_token(jwt_token: str):
     try:
-        payload = jwt.decode(jwt_token, os.environ['DJANGO_SECRET_KEY'], algorithms=["HS256"])
-        return payload 
-    except: 
+        payload = jwt.decode(
+            jwt_token, os.environ['MASTER_SECRET_KEY'], algorithms=["HS256"])
+        return payload
+    except:
         return None
+
 
 def verify_credentials(username: str, password: str):
     try:
-        client = MongoClient('mongodb+srv://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_CLUSTERNAME'] + '.nsb6k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+        client = MongoClient('mongodb+srv://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] +
+                             '@' + os.environ['MONGODB_CLUSTERNAME'] + '.nsb6k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
         database = client['Visser']
         collection = database['users']
-        
+
         user_document = collection.find_one({"username": username})
-        auth = bcrypt.checkpw(str.encode(password), str.encode(user_document['password']))
-        return auth 
+        auth = bcrypt.checkpw(str.encode(password),
+                              str.encode(user_document['password']))
+        return auth
     except:
         return False
