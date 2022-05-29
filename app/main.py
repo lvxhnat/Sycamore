89f8009f652c8dbecbaf2728c776f60d24010d4c
Yi Kuang Loh <yikuang5@gmail.com>
2022-05-29 20:02:09 +0800

    edit(imports: raise httpexception should come from fastapi

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
-from http.client import HTTPException
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
